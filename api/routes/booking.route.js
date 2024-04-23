e2203ec5742954730f8cb71a22d3d9f9db40464e
Nishant-Bharwani <nbtaylor1031@gmail.com>
2024-04-23 23:07:09 +0530

    Middlewares removed

--- api/routes/booking.route.js
+++ api/routes/booking.route.js
@@ -1,12 +1,14 @@
 import express from 'express';
-import { createBooking , getUserBookings} from '../controllers/booking.controller.js';
+import { createBooking, getUserBookings } from '../controllers/booking.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 
-router.route('/book').post(verifyToken, createBooking);
-router.route('/:userId/bookings').get(verifyToken, getUserBookings);
+router.route('/book').post(createBooking);
+router.route('/:userId/bookings').get(getUserBookings);
+// router.route('/book').post(verifyToken, createBooking);
+// router.route('/:userId/bookings').get(verifyToken, getUserBookings);
 
 
 
