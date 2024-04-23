d5a2c82f9ee280f7815414ebcd7dd8047040b76b
Nishant-Bharwani <nbtaylor1031@gmail.com>
2024-04-23 22:52:05 +0530

    Cookie config changed for production

--- api/controllers/booking.controller.js
+++ api/controllers/booking.controller.js
@@ -12,95 +12,95 @@ import UserModel from '../models/user.model.js';
 
 
 export const createBooking = async (req, res, next) => {
-    try {
-        const { userId, listingId, startDate, endDate, bookingPrice } = req.body;
+  try {
+    const { userId, listingId, startDate, endDate, bookingPrice } = req.body;
 
-        if (!userId) {
-            return res.status(400).json({
-                message: "Error fetching user details"
-            });
-        }
-
-        if (!listingId) {
-            return res.status(400).json({
-                message: "Error fetching listing details"
-            });
-        }
-
-        if (!startDate || !endDate) {
-            return res.status(400).json({
-                message: "Start and end dates are required to book"
-            });
-        }
-
-        const user = await UserModel.findById(userId);
-        if (!user) {
-            return res.status(404).json({
-                message: "User does not exist"
-            });
-        }
-        const listing = await ListingModel.findById(listingId);
-        if (!listing) {
-            return res.status(404).json({
-                message: "Listing does not exist"
-            });
-        }
-
-        const formattedStartDate = moment(startDate, 'DD-MM_YYYY').toDate();
-        const formattedEndDate = moment(endDate, 'DD-MM_YYYY').toDate();
-
-        const millisecondsPerDay = 24 * 60 * 60 * 1000;
-        let numberOfDays = Math.ceil((formattedEndDate - formattedStartDate) / (millisecondsPerDay)) + 1;
-      
-        const totalPrice = numberOfDays * listing.discountPrice;
-        console.log(numberOfDays, totalPrice);
-
-        if (bookingPrice != totalPrice) {
-            return res.status(500).json({
-                message: "Something went wrong!!"
-            });
-        }
-
-        const bookingData = {
-            userId,
-            listingId,
-            listingName:listing.name,
-            startDate: formattedStartDate,
-            endDate: formattedEndDate,
-            bookingPrice
-        }
-
-        const booking = await BookingModel.create(bookingData);
-        const razorpay = new Razorpay({
-            key_id: process.env.RAZORPAY_API_KEY,
-            key_secret: process.env.RAZORPAY_API_SECRET,
-        });
-        const razorpayOrder = await razorpay.orders.create({
-            amount: Number(bookingPrice * 100), 
-           
-        });
-        console.log(razorpayOrder);
-        await PaymentModel.create({
-            bookingId: booking._id,
-            amount: bookingPrice,
-            transationId: uuid.v4(),
-            status: 'pending'
-        });
-
-        return res.status(201).json({
-            message: "New Booking created successfully",
-            booking,
-            razorpayOrder
-        });
-    } catch (error) {
-        next(error);
+    if (!userId) {
+      return res.status(400).json({
+        message: "Error fetching user details"
+      });
     }
+
+    if (!listingId) {
+      return res.status(400).json({
+        message: "Error fetching listing details"
+      });
+    }
+
+    if (!startDate || !endDate) {
+      return res.status(400).json({
+        message: "Start and end dates are required to book"
+      });
+    }
+
+    const user = await UserModel.findById(userId);
+    if (!user) {
+      return res.status(404).json({
+        message: "User does not exist"
+      });
+    }
+    const listing = await ListingModel.findById(listingId);
+    if (!listing) {
+      return res.status(404).json({
+        message: "Listing does not exist"
+      });
+    }
+
+    const formattedStartDate = moment(startDate, 'DD-MM_YYYY').toDate();
+    const formattedEndDate = moment(endDate, 'DD-MM_YYYY').toDate();
+
+    const millisecondsPerDay = 24 * 60 * 60 * 1000;
+    let numberOfDays = Math.ceil((formattedEndDate - formattedStartDate) / (millisecondsPerDay)) + 1;
+
+    const totalPrice = numberOfDays * listing.discountPrice;
+    console.log(numberOfDays, totalPrice);
+
+    if (bookingPrice != totalPrice) {
+      return res.status(500).json({
+        message: "Something went wrong!!"
+      });
+    }
+
+    const bookingData = {
+      userId,
+      listingId,
+      listingName: listing.name,
+      startDate: formattedStartDate,
+      endDate: formattedEndDate,
+      bookingPrice
+    }
+
+    const booking = await BookingModel.create(bookingData);
+    const razorpay = new Razorpay({
+      key_id: process.env.RAZORPAY_API_KEY,
+      key_secret: process.env.RAZORPAY_API_SECRET,
+    });
+    const razorpayOrder = await razorpay.orders.create({
+      amount: Number(bookingPrice * 100),
+
+    });
+    console.log(razorpayOrder);
+    await PaymentModel.create({
+      bookingId: booking._id,
+      amount: bookingPrice,
+      transationId: uuid.v4(),
+      status: 'pending'
+    });
+
+    return res.status(201).json({
+      message: "New Booking created successfully",
+      booking,
+      razorpayOrder
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const getUserBookings = async (req, res, next) => {
   try {
-    const userId = req.user.id; 
-    
+    const userId = req.user.id;
+
     if (!userId) {
       return res.status(400).json({
         message: "User ID is required",
@@ -113,9 +113,9 @@ export const getUserBookings = async (req, res, next) => {
         message: "User not found",
       });
     }
- 
+
     const bookings = await BookingModel.find({ userId })
-      .populate('listingId') 
+      .populate('listingId')
       .exec();
 
     if (!bookings || bookings.length === 0) {
@@ -126,11 +126,11 @@ export const getUserBookings = async (req, res, next) => {
 
     const bookingsWithListings = bookings.map((booking) => ({
       _id: booking._id,
-      listingName:booking.listingName,
+      listingName: booking.listingName,
       startDate: booking.startDate,
       endDate: booking.endDate,
       bookingPrice: booking.bookingPrice,
-      listing: booking.listingId, 
+      listing: booking.listingId,
     }));
 
     return res.status(200).json({
